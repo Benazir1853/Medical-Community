47936d6a9f6c41660b34958fe68fef36c242a980
Ha-Mim <hamim.hyder@gmail.com>
2015-01-28 22:43:42 +0600

    New File Added

---
+++ MedicalCommunityAutomation/MedicalCommunityAutomation.DAL/DbGateway.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+
+namespace MedicalCommunityAutomation.DAL
+{
+    public class DbGateway
+    {
+        private string connectionString =
+            WebConfigurationManager.ConnectionStrings["connectionStringForMedicalCommunityDb"].ConnectionString;
+
+        public string ConnectionString
+        {
+            get { return connectionString; }
+            set { connectionString = value; }
+        }
+
+        public SqlConnection ASqlConnection { set; get; }
+        public SqlCommand ASqlCommand { set; get; }
+        public SqlDataReader ASqlDataReader { set; get; }
+
+        public DbGateway()
+        {
+            ASqlConnection = new SqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ MedicalCommunityAutomation/MedicalCommunityAutomation/doctorEntry.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MedicalCommunityAutomation.BLL;
+using MedicalCommunityAutomation.DAO;
+
+namespace MedicalCommunityAutomation
+{
+    public partial class doctorEntry : System.Web.UI.Page
+    {
+        DoctorManager aDoctorManager = new DoctorManager();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            Doctor aDoctor = new Doctor();
+            aDoctor.Name = nameTextBox.Text;
+            aDoctor.Degree = degreeTextBox.Text;
+            aDoctor.Specialization = specializationTextBox.Text;
+            //aDoctor.CenterId = (int)Session["Center_Id"];
+            string msg=aDoctorManager.Save(aDoctor);
+            msgLabel.Text = msg;
+        }
+    }
+}
